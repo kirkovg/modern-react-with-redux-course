cdf0b12564ea54d1e066a6c0d79caafbdc534290
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2019-07-10 22:55:32 +0200

    [modern-react-course] Section 1 - 12 Displaying content with functional components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class App extends React.Component {
-
-  render() {
-    return (
-      <div>
-        tocak
-      </div>
-    );
-  }
-}
+const App = () => <div>Hi there</div>;
 
 ReactDOM.render(
   <App/>,
