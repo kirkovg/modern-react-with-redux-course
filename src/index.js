363d622e5383ebc5d6b4296b82561649d3df8b26
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2019-07-11 20:21:39 +0200

    [modern-react-course] Section 2 - finished section 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>Hi there</div>;
+
+const App = () => {
+  const buttonText = {
+    text: 'tocak'
+  };
+  const labelName = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">{labelName}</label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'red', color: 'white'}}>
+        {Object.values(buttonText)}
+      </button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <App/>,
