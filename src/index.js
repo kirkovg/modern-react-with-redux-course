fd1608750ef62682340a89bcb6a31488c3ba2774
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2019-07-16 22:38:47 +0200

    [modern-react-course] Section 5 - finished section 5 (state in react components)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    this.state = { lat: null, lng: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      ({coords}) => this.setState({lat: coords.latitude, lng: coords.longitude}),
+      (error) => this.setState({errorMessage: error.message}));
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div>
+          Error: {this.state.errorMessage}
+        </div>
+      );
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div>
+          Latitude: {this.state.lat}, Latitude: {this.state.lng} <br/>
+        </div>
+      );
+    }
+    return <div>Loading...</div>;
+  }
+}
+
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+);
