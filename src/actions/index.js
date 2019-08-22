61eb5819f77d1ffbe95f756bdc413a02c2633c82
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2019-08-22 23:22:26 +0200

    [modern-react-course] Section 15 - Finished Redux store design

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,23 @@
+import * as _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceholder.get('./posts');
-    dispatch({type: 'FETCH_POSTS', payload: response});
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
+
+const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({type: 'FETCH_POSTS', payload: response.data});
+};
+
+const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({type: 'FETCH_USER', payload: response.data});
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from "./usersReducer";
 
 
 export default combineReducers({
-  replaceMe: () => 'vogazo'
+  posts: postsReducer,
+  users: usersReducer
 });
